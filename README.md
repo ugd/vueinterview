6fb4f2f4ef42e151ecc09aeaae0023e47a0b5608
UGD <undergrounddomain@yahoo.com>
2022-08-26 22:36:30 +0300

    Finished CRUD

--- README.md
+++ README.md
@@ -2,40 +2,13 @@
 
 Marian-Bogdan Muntean's project
 
-## Install the dependencies
-```bash
-yarn
-# or
-npm install
-```
-
-### Start the app in development mode (hot-code reloading, error reporting, etc.)
-```bash
-quasar dev
-```
-
-
-### Lint the files
-```bash
-yarn lint
-# or
-npm run lint
-```
-
-
-### Format the files
-```bash
-yarn format
-# or
-npm run format
-```
-
-
-
-### Build the app for production
-```bash
-quasar build
-```
-
-### Customize the configuration
-See [Configuring quasar.config.js](https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js).
+## App Requirements
+VueJS cu vuetify sau quasar
+O aplicatie ce contine o lista in care se vor salva obiective turistice pentru web+mobile (responsive). Adaugarea se va face printr-un popup ce va contine urmatoarele campuri:
+- Titlu - required
+- Poza (url din random online). Camp text la salvare in popup, randare poza in lista
+- Oras dintr-un dropdown - required API
+orase: https://api.geoapify.com/v1/geocode/search?text=Brail&apiKey=dcbd199fc745414a8a9a9770 21537cbd
+Lista sa fie editabila inline pe campurile de mai sus.
+In plus, lista trebuie sa aiba pe fiecare rand bifa de vizitat si optiune de adaugare rating (stelute). Sa se adauge sortare lista pe baza ratingului si filtrare pe oras.
+Totul se salveaza in local storage. Timp de rezolvare – 4 ore.
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <q-btn>Adauga</q-btn>
+    <q-btn v-on:click="addObjective()" color="green">Salveaza</q-btn>
+    <q-input v-model="titleInput" label="Titlu" />
+    <q-input v-model="urlPhoto" label="Poza" />
+    <q-select v-model="selectedCity" :options="filteredCities" label="Standard" />
+    <q-img
+      style="border-radius: 10px; width: 10em; height: 6em"
+      src="https://picsum.photos/200/300"
+    />
+    <div v-for="(objective, index) in objectives" v-bind:key="index">
+      <q-btn v-on:click="removeObjective(index)" color="red">Sterge</q-btn>
+      <q-btn v-on:click="updateObjective(objective)" color="blue">Salveaza</q-btn>
+      <!-- <q-btn v-on:click="updateObjective(objective.id,objective.title,objective.)" color="blue">Salveaza</q-btn> -->
+
+      <q-input v-model="objective.title" label="Titlu" />
+      <q-select
+        v-model="objective.selectedCity"
+        :options="filteredCities"
+        label="Standard"
+      />
+      <q-checkbox label="Vizitat" v-model="objective.checkbox" />
+      <q-rating
+        v-model="objective.rating"
+        :max="5"
+        size="3.5em"
+        color="green-5"
+        :icon="icons"
+      />
+      <q-img
+        style="border-radius: 10px; width: 10em; height: 6em"
+        src="https://picsum.photos/200/300"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { LocalStorage } from "quasar";
+import axios from "axios";
+export default {
+  name: "MainPage",
+  data() {
+    return {
+      id: 0,
+      checkbox: false,
+      urlPhoto: "",
+      titleInput: "",
+      citiesResponse: null,
+      rating: 0,
+      message: "",
+      selectedCity: "",
+      objectives: LocalStorage.getItem("objectives") || [],
+    };
+  },
+  methods: {
+    addObjective() {
+      //add to local storage the new objective as an object with strigified data
+      //check if local storage exists
+      let lastId = 0
+      if (this.objectives.length > 0) {
+         lastId = this.objectives[this.objectives.length - 1].id + 1;
+      }
+      let newObjective = {
+        id: lastId,
+        title: this.titleInput,
+        urlPhoto: this.urlPhoto,
+        selectedCity: this.selectedCity,
+        checkbox: false,
+        rating: 0,
+      };
+      this.objectives.push(newObjective);
+      LocalStorage.set("objectives", this.objectives);
+
+      console.log("addObjective");
+    },
+    updateObjective(newObjective) {
+      let toUpdateObjective = this.objectives.find((objective) => objective.id == newObjective.id);
+      toUpdateObjective = newObjective;
+      LocalStorage.set("objectives", this.objectives);
+    },
+    removeObjective(index) {
+      this.objectives.splice(index, 1);
+      LocalStorage.set("objectives", this.objectives);
+      console.log("removeObjective");
+    },
+  },
+  mounted() {
+    let romanianCountries =
+      "https://countriesnow.space/api/v0.1/countries/population/cities/filter";
+    let postData = {
+      limit: 10,
+      order: "asc",
+      orderBy: "name",
+      country: "romania",
+    };
+
+    axios.post(romanianCountries, postData).then((response) => {
+      this.citiesResponse = response.data;
+    });
+  },
+  computed: {
+    filteredCities: function () {
+      if (this.citiesResponse) {
+        return this.citiesResponse.data.map((element) => {
+          return element.city;
+        });
+        return this.cities;
+      }
+      return ["Bucuresti", "Cluj", "Iasi", "Timisoara"];
+    },
+    objectivesComputed: function () {
+      var x = LocalStorage.getItem("objectives") || [];
+      var y = x;
+      return x;
+    },
+  },
+};
+</script>
+
+<style></style>
